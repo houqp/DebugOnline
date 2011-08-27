ebc1896292f3e0c58f1f1b72935e5f40fcccd56f
root <root@beihaison-ThankPad.(none)>
2011-08-27 17:09:00 +0800

    system.php template

--- system.php
+++ system.php
@@ -1,15 +1,38 @@
 <?php
 	$code = $_POST["code"];
 	$title = $_POST["title"];
-	$compile = $_POST["compile"];
-	$run = $_POST["run"];
 	$debug = $_POST["debug"];
 	$debugs = $_POST["debugs"];
 	$next = $_POST["next"];
+	$action = $_POST["action"];
 	
-	$dir = "var/www/debugonline/";
+	$dir = "var/www/debugonline/temp/";
 	$filename = $dir.$title;
 	$fp = fopen($filename,"w");
-	fputs($code,$fp);
+	fputs($fp,$code);
+	fclose($fp);
 	
+	if(action == "run")
+	{	
+		//留待完善
+		$command = ;
+		shell_exec($command);
+		$resultfile = $dir."";
+	}
+	else if(action == "compile")
+	{
+		$command = ;
+		shell_exec($command);
+	}
+	else if(action == "debug")
+	{
+		$command = ;
+		shell_exec($command);
+	}
+	
+	function getResult($file);
+	{
+		$result = file_get_contents($fp);
+		echo $result;		
+	}
 ?>
