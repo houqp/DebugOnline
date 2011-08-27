bcd1997ceef7a588fb4910e77a4a745a3e1896d6
root <root@beihaison-ThankPad.(none)>
2011-08-27 19:34:45 +0800

    添加前台html文本以及处理不同action的不同操作

--- system.php
+++ system.php
@@ -4,35 +4,47 @@
 	$debug = $_POST["debug"];
 	$debugs = $_POST["debugs"];
 	$next = $_POST["next"];
-	$action = $_POST["action"];
+	$action = $_GET["action"];
+	$debugline = $_POST["debugline"];
+	$barname = $_POST["barname"];
+	function getResult($file)
+	{
+		return $result = file_get_contents($file);		
+	}
+	//echo $code.$title.$debugline.$barname;
 	
-	$dir = "var/www/debugonline/temp/";
+	//echo $action;	
+	$dir = "/var/www/debugonline/temp/";
 	$filename = $dir.$title;
 	$fp = fopen($filename,"w");
 	fputs($fp,$code);
 	fclose($fp);
-	
-	if(action == "run")
+	if($action == "run")
 	{	
-		//留待完善
-		$command = "make run" ;
-		shell_exec($command);
-		$resultfile = $dir."";
+		$command = "cd temp && make run" ;
+		//$command = "ls";
+		exec($command);
+		$resultfile = $dir."run_output";
+		getResult($resultfile);
 	}
-	else if(action == "compile")
+	else if($action == "compile")
 	{
-		$command = "make";
+		$command = "cd temp && make";
 		shell_exec($command);
+		$resultfile = $dir."compile_output";
+		$result = getResult($resultfile);
+		if($result == "")
+			echo "compile good";
+		else echo $result;
+		
 	}
-	else if(action == "debug")
-	{
+	else if($action == "debug")
+	{	
 		$command = "make gdb";
 		shell_exec($command);
 	}
 	
-	function getResult($file);
-	{
-		$result = file_get_contents($fp);
-		echo $result;		
-	}
+	
+	
+	
 ?>
