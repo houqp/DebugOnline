58fe0354871684c05f4e4c548cdf3c7de5179ad0
Qingping Hou <dave2008713@gmail.com>
2011-08-28 15:21:34 +0800

    style draft

--- index.php
+++ index.php
@@ -1,36 +1,32 @@
 <html>
 <head>
-<title>
-debugonline
-</title>
-<script type="text/javascript" src="scripts/ajax.js"></script>
+  <title>debugonline</title>
+  <link rel="stylesheet" type="text/css" href="style.css"/>
+  <script type="text/javascript" src="scripts/ajax.js"></script>
 </head>
 <body>
-
-<form action="system.php" method="POST" name="myForm">
-code title:<input type="text" value="main.c" name="title" id="title">
-<p>
-<textarea type="submit" value="submit" name="code"  id="code" rows="10" cols="32">
-	#include<stdio.h>
-	int main()
-	{
-		int a=0;
-		a=10;
-		printf("hello world");
-		return 0;
-	}
-</textarea>
-<p>
-<div id="debug">
-	<input type="button" value="compile" onClick="checksubmit('compile');"/>
-	<input type="button" value="debug" onClick="change();"/>
-	<input type="button" value="run" onClick="checksubmit('run');"/>
-</div>
-<p>
-<div id="result">
-</div>
-</br>
-</form>
+  <form action="system.php" method="POST" name="myForm">
+  <p>code title:</p>
+  <input type="text" value="main.c" name="title" id="title">
+  <textarea type="submit" value="submit" name="code"  id="code" rows="10" cols="32">
+    #include<stdio.h>
+    int main()
+    {
+      int a=0;
+      a=10;
+      printf("hello world");
+      return 0;
+    }
+  </textarea>
+  <div id="debug">
+    <input type="button" value="compile" onClick="checksubmit('compile');"/>
+    <input type="button" value="run" onClick="checksubmit('run');"/>
+    <input type="button" value="debug" onClick="change();"/>
+  </div>
+  <div id="result">
+  </div>
+  </br>
+  </form>
 
 </body>
 </html>
